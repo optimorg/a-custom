46184e04a61153457451a6ef941347b152885927
gotoworq <110043355+gotoworq@users.noreply.github.com>
2023-02-16 12:15:53 -0500

    initial spin up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInFormComponent } from './log-in-form/log-in-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'log-in-form-component', component: LogInFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogInFormComponent } from './log-in-form/log-in-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogInFormComponent
   ],
   imports: [
     BrowserModule,
