e201cc066e7d8150b7cc61edb2fddf750bacca16
morgREEN <morgreen25@gmail.com>
2023-03-13 18:52:07 -0400

    Began building registratio form, having issues with linking the pages from login page to new user registration form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogInFormComponent } from './log-in-form/log-in-form.component';
 import { SplashMainComponent } from './splash-main/splash-main.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogInFormComponent,
-    SplashMainComponent
+    SplashMainComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
