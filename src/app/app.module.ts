a8e1cf8702f1f11a26dc6f4f983f432c9927776d
morgREEN <morgreen25@gmail.com>
2023-03-17 16:59:41 -0400

    Styling edits
    Added some changes to the login form's css and added registration form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,26 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { LogInFormComponent } from './log-in-form/log-in-form.component';
+<<<<<<< HEAD
 import { SplashMainComponent } from './splash-main/splash-main.component';
 import { SignupFormComponent } from './signup-form/signup-form.component';
 import { DesignerDashboardComponent } from './designer-dashboard/designer-dashboard.component';
+=======
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+>>>>>>> origin/login-page
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogInFormComponent,
+<<<<<<< HEAD
     SplashMainComponent,
     SignupFormComponent,
     DesignerDashboardComponent
+=======
+    RegistrationFormComponent
+>>>>>>> origin/login-page
   ],
   imports: [
     BrowserModule,
