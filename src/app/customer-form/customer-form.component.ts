3ae28f25a08f0e1c9bb15b743c8deb2b88a1f102
gotoworq <110043355+gotoworq@users.noreply.github.com>
2023-03-21 09:17:36 -0400

    added some material form elements

--- src/app/customer-form/customer-form.component.ts
+++ src/app/customer-form/customer-form.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  value: string;
+  viewValue: string;
+}
+
 @Component({
   selector: 'app-customer-form',
   templateUrl: './customer-form.component.html',
   styleUrls: ['./customer-form.component.css']
 })
 export class CustomerFormComponent {
-
+  products: Product[] = [
+    { value: 'nail-0', viewValue: 'Black and Gold' },
+    { value: 'nail-1', viewValue: 'Blue and Orange' },
+    { value: 'nail-2', viewValue: 'White and Red' },
+  ];
 }
