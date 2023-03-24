5685288e0cf3905b9e5bdf2e4c47b57cd53890a6
gotoworq <110043355+gotoworq@users.noreply.github.com>
2023-03-24 09:45:25 -0400

    fixed routing issue

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { CustomerFormComponent } from './customer-form/customer-form.component';
 import { LogInFormComponent } from './log-in-form/log-in-form.component';
 import { SplashMainComponent } from './splash-main/splash-main.component';
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 const routes: Routes = [
   { path: 'log-in-form-component', component: LogInFormComponent },
   { path: 'customer-form-component', component: CustomerFormComponent },
   { path: 'splash-main-component', component: SplashMainComponent },
-  { path: '', redirectTo: '/splash-main-component', pathMatch: 'full' }
-import { RegistrationFormComponent } from './registration-form/registration-form.component';
-
-const routes: Routes = [
-  { path: 'log-in-form-component', component: LogInFormComponent },
+  { path: '', redirectTo: '/splash-main-component', pathMatch: 'full' },
   { path: 'registration-form-component', component: RegistrationFormComponent }
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
