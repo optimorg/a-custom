aac6ea0166ff35341398d22051edb862c6a49e18
morgREEN <morgreen25@gmail.com>
2023-04-24 18:19:00 -0400

    "Added navigation to dashboard"

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+  { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
+  { path: '/user-profile', title: 'User Profile',  icon:'person', class: '' },
+  { path: '/availability', title: 'Availability',  icon:'library_books', class: '' },
+  { path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
+];
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  dashTabs: any[];
 
+  constructor() { }
+  
+  ngOnInit() {
+    this.dashTabs = ROUTES.filter(dashTab => dashTab);
+  }
 }
+
+
